090166a8b5430d5ca773b65ae7c33ee0931f4d3e
dannypopov <jlpopov13@gmail.com>
2018-04-22 14:31:23 +0300

    initial host page and add post servlet

--- src/controller/UploadPostServlet.java
+++ src/controller/UploadPostServlet.java
@@ -1,6 +1,8 @@
 package controller;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.time.LocalDate;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,23 +10,37 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import exceptions.InvalidPostDataExcepetion;
+import manager.PostManager;
+import model.Post;
+import model.User;
+
 @WebServlet("/upload")
 public class UploadPostServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		User currUser = (User) request.getSession().getAttribute("user");
 		// not logged
-		// if (request.getSession().getAttribute("user") == null) {
-		request.getRequestDispatcher("login.jsp").forward(request, response);
-		// }
-		String title = request.getParameter("title");
-		String description = request.getParameter("description");
-		// int price = Integer.valueOf(request.getParameter("price"));
-		String 	type = request.getParameter("type");
-		System.out.println(title + " " + description + " " + " " + type);
+		if (currUser == null) {
+			System.out.println("not logged");
+			request.getRequestDispatcher("login.jsp").forward(request, response);
+		} else {
 
-		// logged
-		// TODO GET USER ID FROM DB
+			System.out.println("logged");
+			String title = request.getParameter("title");
+			String description = request.getParameter("description");
+			int price = Integer.valueOf(request.getParameter("price"));
+			String type = request.getParameter("type");
+			int hostID = 23; // for the moment !
+			// TODO ADD PHOTO
+			try {
+				Post newPost = new Post(title, description, price, LocalDate.now(), Post.Type.getType(type));
+				PostManager.instance.insertPost(newPost);
+			} catch (InvalidPostDataExcepetion | SQLException e) {
+				e.printStackTrace();
+			}
+		}
 
 	}
 
